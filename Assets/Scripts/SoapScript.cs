bdcb90126bfea12c69a035af9aa3197d3355d23e
SixtyGig <38166922+SixtyGig@users.noreply.github.com>
2018-11-28 17:21:34 -0600

    WashHands/DryHands/Music/Walls
    -WashHands: Now checks list when player approaches sink
    -Paper Towel: Checks list
    -Music: SHOULD work
    -Walls: Now have bigger boxes, to prevent ANY sort of flying out

---
+++ Assets/Scripts/SoapScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoapScript : MonoBehaviour {
+    public GameObject handWashStation;
+
+	// Use this for initialization
+	void Awake () {
+		
+	}
+
+    // Update is called once per frame
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Debug.Log("Hands have been washed");
+            GameObject.FindGameObjectWithTag("Table").GetComponent<CheckListChecker>().handsDried = true;
+        }
+    }
+}
