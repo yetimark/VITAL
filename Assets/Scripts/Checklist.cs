6e0e6cf830f30d482b614cf5537ccfce0c05b0fb
SixtyGig <38166922+SixtyGig@users.noreply.github.com>
2019-06-12 19:15:35 -0500

    ChecklistRework

---
+++ Assets/Scripts/Checklist.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checklist : MonoBehaviour
+{
+    private GameObject txtboxAdjuster;
+    private int index = 0;
+
+    public void Awake()
+    {
+        txtboxAdjuster = GameObject.Find("checkspot_" + index);
+    }
+}
