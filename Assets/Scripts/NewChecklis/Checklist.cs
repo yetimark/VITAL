bcf23d0584cabb9053e5c42251ad728172164461
SixtyGig <38166922+SixtyGig@users.noreply.github.com>
2019-06-15 00:02:46 -0500

    Work

--- Assets/Scripts/NewChecklis/Checklist.cs
+++ Assets/Scripts/NewChecklis/Checklist.cs
@@ -5,34 +5,54 @@ using UnityEngine;
 public class Checklist : MonoBehaviour
 {
     Supplies newSupply = new Supplies();
-    private Dictionary<int, object> checkList = new Dictionary<int, object>();
+    private Dictionary<string, bool> checkList = new Dictionary<string, bool>();
 
     private GameObject checklistPanel, txtboxAdjuster;
     private int index = 0;
-
 
     public void Awake()
     {
         this.checklistPanel = GameObject.Find("CheckListPanel");
         txtboxAdjuster = GameObject.Find("checkspot_" + index);
 
-
+        
     }
 
     private void CreateSupplyList_PiccLine()
     {
-        this.checkList.Add(0, newSupply.name);
-        this.checkList.Add(1, false);
-        this.checkList.Add(2, false);
-        this.checkList.Add(3, false);
-        this.checkList.Add(4, false);
-        this.checkList.Add(5, false);
-        this.checkList.Add(6, false);
-        this.checkList.Add(7, false);
-        this.checkList.Add(8, false);
-        this.checkList.Add(9, false);
-        this.checkList.Add(10, false);
+        
+        this.checkList.Add("Dressing Change Kit", s0);
+        this.checkList.Add("Face Mask for Patient", false);
+        this.checkList.Add("Securement Device", false);
+        this.checkList.Add("Sterile 4x4", false);
+        this.checkList.Add("Sterile Drape", false);
+        this.checkList.Add("Sterile Gloves", false);
+        this.checkList.Add("Tegaderm", false);
+        this.checkList.Add("Alcohol Wipes", false);
+        this.checkList.Add("Biopatch", false);
+        this.checkList.Add("Chux", false);
+        this.checkList.Add("Clean Gloves", false);
         //this.checkList.Add(11, obj);
         Debug.Log("PiccLine Checklist Created");
+    }
+
+    private void NewToTable(string objectName)
+    {
+        if (GameObject.Find(objectName).tag != "Player")
+        {
+            if (this.checkList.ContainsKey(objectName) && this.checkList[objectName] == false) // If key is not in dictionary, will it be false for part two if it was the only one?
+            {
+                this.checkList[objectName] = true;
+                this.truthCounter++;
+                Debug.Log("truthCounter" + truthCounter);
+
+                gameUI.GetComponent<UIGame>().GoodAction();
+            }
+            else
+            {
+                gameUI.GetComponent<UIGame>().BadAction();
+                GameObject.Find(objectName).GetComponent<boxReturn>().ReturnHome(); // return object to shelf
+            }
+        }
     }
 }
--- Assets/Scripts/NewChecklis/Supplies.cs
+++ Assets/Scripts/NewChecklis/Supplies.cs
@@ -6,10 +6,18 @@ public class Supplies : MonoBehaviour
 {
     private int index = 0;
     private string myName = "";
+    private bool imageState = false;
 
-    public Supplies(int location, string myName)
+    public Supplies(int index, bool imageState)
     {
-        this.myName = myName;
+        this.index = index;
+        this.imageState = imageState;
     }
+
+    //public void SetIndex(int newIndex) { this.index = newIndex }
+    //public int GetIndex() { return this.index; }
+
+    //public void SetImageState(bool newImageState) { this.imageState = newImageState; }
+    //public bool GetImageState() { return this.imageState; }
 
 }
