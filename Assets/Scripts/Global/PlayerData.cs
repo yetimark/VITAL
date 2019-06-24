478e00c3f43d86f19c5aa0a04f7640ae31cdeeb8
SixtyGig <38166922+SixtyGig@users.noreply.github.com>
2019-06-23 23:14:27 -0500

    Saving Backend

---
+++ Assets/Scripts/Global/PlayerData.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using UnityEngine;
+
+public class PlayerData : MonoBehaviour
+{
+    //This class holds session data, and loads/saves to the saved data set
+
+    public string roomName;
+    public string difficulty;
+    public int playerStrikes;
+    public int playerPoints;
+
+    public bool supplyRoom;
+    public bool medRoom;
+    public bool skillsRoom;
+
+    public PlayerDataSet saveData;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        saveData = new PlayerDataSet();
+        Load();
+    }
+
+    //saves data to file
+    public void Save()
+    {
+        saveData.roomName = roomName;
+        saveData.difficulty = difficulty;
+        saveData.playerStrikes = playerStrikes;
+        saveData.playerPoints = playerPoints;
+
+        saveData.supplyRoom = supplyRoom;
+        saveData.medRoom = medRoom;
+        saveData.skillsRoom = skillsRoom;
+
+        string destination = Application.persistentDataPath + "/save.dat";
+        FileStream file;
+
+        if (File.Exists(destination)) file = File.OpenWrite(destination);
+        else file = File.Create(destination);
+
+        BinaryFormatter bf = new BinaryFormatter();
+        bf.Serialize(file, saveData);
+        file.Close();
+    }
+
+    //loads data to session
+    public void Load()
+    {
+        string destination = Application.persistentDataPath + "/save.dat";
+        FileStream file;
+
+        if (File.Exists(destination)) file = File.OpenRead(destination);
+        else
+        {
+            GenerateDefaults();
+            Save();
+            file = File.OpenRead(destination);
+        }
+
+        BinaryFormatter bf = new BinaryFormatter();
+        saveData = (PlayerDataSet)bf.Deserialize(file);
+        file.Close();
+
+        roomName = saveData.roomName;
+        difficulty = saveData.difficulty;
+        playerStrikes = saveData.playerStrikes;
+        playerPoints = saveData.playerPoints;
+
+        supplyRoom = saveData.supplyRoom;
+        medRoom = saveData.medRoom;
+        skillsRoom = saveData.skillsRoom;
+
+        SetStats();
+    }
+
+
+    //sets default values
+    public void GenerateDefaults()
+    {
+        roomName = saveData.roomName;
+        difficulty = saveData.difficulty;
+        playerStrikes = saveData.playerStrikes;
+        playerPoints = saveData.playerPoints;
+
+        supplyRoom = saveData.supplyRoom;
+        medRoom = saveData.medRoom;
+        skillsRoom = saveData.skillsRoom;
+    }
+
+    //updates stats at runtime
+    public void SetStats()
+    {
+        GetComponent<GameController>().playerHealth = totalHealth;
+        GetComponent<GameController>().playerPower = totalAmmo;
+        GetComponent<GameController>().playerMaxHealth = totalHealth;
+        GetComponent<GameController>().playerMaxPower = totalAmmo;
+    }
+}
--- Assets/Scripts/Global/PlayerDataSet.cs
+++ Assets/Scripts/Global/PlayerDataSet.cs
@@ -2,18 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerInfo : MonoBehaviour
+[System.Serializable]
+public class PlayerDataSet
 {
     public string roomName;
     public string difficulty;
     public int playerStrikes;
     public int playerPoints;
 
-    public void Start()
-    {
-        this.roomName = "Menu";
-        this.difficulty = "Null";
-        this.playerStrikes = 0;
-        this.playerPoints = 0;
-    }
+    public bool supplyRoom;
+    public bool medRoom;
+    public bool skillsRoom;
 }
