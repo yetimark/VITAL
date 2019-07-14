b8d150ab11c4ad8e71b0f1f384061fe64f04204b
SixtyGig <38166922+SixtyGig@users.noreply.github.com>
2019-07-14 02:52:43 -0500

    Checklist Changes

--- Assets/Scripts/Rooms/SupplyRoom/Supplies/SupplyLocations.cs
+++ Assets/Scripts/Rooms/SupplyRoom/Supplies/SupplyLocations.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SupplyLocationID : MonoBehaviour
+public class SupplyLocations : MonoBehaviour
 {
+    // The ID associated with each shelf location
     public int SupplyLocationID;
 }
